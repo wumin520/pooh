3befb8e119a55961139c55a525e0558b356105ac
叶俊 <ye.jun@qianka.com>
2017-07-17 17:33:04 +0800

    update

--- .postcssrc.js
+++ .postcssrc.js
@@ -3,6 +3,8 @@
 module.exports = {
   "plugins": {
     // to edit target browsers: use "browserslist" field in package.json
-    "autoprefixer": {}
+    "autoprefixer": {
+      "browsers": ['last 3 versions', 'safari >= 7']
+    }
   }
 }
