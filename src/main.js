2b6f598e59788d416eb19bed449d848edca02cbd
叶俊 <ye.jun@qianka.com>
2017-07-18 10:59:14 +0800

    add element-ui & qianka-theme

--- src/main.js
+++ src/main.js
@@ -1,5 +1,8 @@
 import 'normalize.css/normalize.css'
-import 'element-ui/lib/theme-default/index.css'
+// import 'element-ui/lib/theme-default/index.css'
+// 使用定制的 element 钱咖主题
+import 'element-theme-qianka/dist/index.css'
+import 'element-theme-qianka/dist/qianka.css'
 
 // Polyfill
 import 'whatwg-fetch'
@@ -8,12 +11,14 @@ Object.assign = assign
 
 // Underground
 import Vue from 'vue'
+import ElementUI from 'element-ui'
 import App from './App'
 import router from './router'
 import store from './store'
 import {sync} from 'vuex-router-sync'
 sync(store, router, 'router')
 
+Vue.use(ElementUI)
 Vue.config.productionTip = false
 
 /* eslint-disable no-new */
--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -1,5 +1,21 @@
 <template>
   <div>
     <h1> dashboard goes here.</h1>
+
+    <el-button @click="onClick">默认按钮</el-button>
   </div>
 </template>
+
+<script>
+  export default {
+    methods: {
+      onClick () {
+        this.$notify({
+          title: 'Tips',
+          message: 'Hello, Element!',
+          duration: 0
+        })
+      }
+    }
+  }
+</script>
