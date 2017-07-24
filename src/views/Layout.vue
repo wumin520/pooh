050d74e015eecb1c33bce39b913adf4694aae27e
Hururu <hu.ruru@qianka.com>
2017-07-24 11:37:27 +0800

    sidebar 调整

--- src/views/Layout.vue
+++ src/views/Layout.vue
@@ -25,14 +25,18 @@
           style="width: 180px; padding-top: 32px;">
           <el-menu-item index="1"><i class="el-icon-square"></i>账户首页</el-menu-item>
 
-          <el-submenu index="2">
+          <el-menu-item index="2"><i class="el-icon-square"></i>iOS广告</el-menu-item>
+
+          <el-menu-item index="2-1" style="padding-left:44px;">添加新广告</el-menu-item>
+
+          <!--<el-submenu index="2">
             <template slot="title"><i class="el-icon-square"></i>iOS 广告</template>
             <el-menu-item-group>
               <el-menu-item index="2-1">iOS广告</el-menu-item>
               <el-menu-item index="2-2">新加广告</el-menu-item>
               <el-menu-item index="2-3">备选项</el-menu-item>
             </el-menu-item-group>
-          </el-submenu>
+          </el-submenu> --> 
 
           <el-menu-item index="3"><i class="el-icon-square"></i>财务管理</el-menu-item>
 
@@ -85,10 +89,10 @@
           case '1':
             this.$router.push('/d/home')
             break
-          case '2-1':
+          case '2':
             this.$router.push('/d/ad')
             break
-          case '2-2':
+          case '2-1':
             this.$router.push('/d/ad/new')
             break
           case '3':
