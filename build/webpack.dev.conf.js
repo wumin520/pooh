86889139da29bdb04abed2b065106ca9531bc9d7
叶俊 <ye.jun@qianka.com>
2017-08-03 18:37:18 +0800

    update flotr2

--- build/webpack.dev.conf.js
+++ build/webpack.dev.conf.js
@@ -27,8 +27,9 @@ module.exports = merge(baseWebpackConfig, {
     // https://github.com/ampedandwired/html-webpack-plugin
     new HtmlWebpackPlugin({
       filename: 'index.html',
-      template: 'index.html',
-      inject: true
+      template: 'index.ejs',
+      inject: true,
+      cdn: '/'
     }),
     new FriendlyErrorsPlugin()
   ]
