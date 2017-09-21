908159c4b3459f7c9731688cecdceb138a2d52e2
史俊杰 <shi.junjie@qianka.com>
2017-09-21 17:50:04 +0800

    参数名字修改

--- src/store/gaoe/user.js
+++ src/store/gaoe/user.js
@@ -175,7 +175,7 @@ const actions = {
     let now = parseInt((new Date()).getTime() / 1000)
     let startTime = now - dayCnt * 86400
     let endTime = now - 86400
-    let url = `${URI_HI_REPORT}?action=chart&content=${content}&day_cnt=${dayCnt}&ts_start=${startTime}&ts_end=${endTime}`
+    let url = `${URI_HI_REPORT}?action=chart&content=${content}&day_cnt=${dayCnt}&start_time=${startTime}&end_time=${endTime}`
 
     return fetch(url, {
       credentials: 'same-origin'
@@ -193,7 +193,7 @@ const actions = {
     let now = parseInt((new Date()).getTime() / 1000)
     let startTime = now - dayCnt * 86400
     let endTime = now - 86400
-    let url = `${URI_HI_REPORT}?action=table&content=${content}&ts_start=${startTime}&ts_end=${endTime}`
+    let url = `${URI_HI_REPORT}?action=table&content=${content}&start_time=${startTime}&end_time=${endTime}`
 
     return fetch(url, {
       credentials: 'same-origin'
@@ -211,7 +211,7 @@ const actions = {
     let now = parseInt((new Date()).getTime() / 1000)
     let startTime = now - dayCnt * 86400
     let endTime = now - 86400
-    location.href = `${URI_HI_REPORT_DOWNLOAD}?ts_start=${startTime}&ts_end=${endTime}`
+    location.href = `${URI_HI_REPORT_DOWNLOAD}?start_time=${startTime}&end_time=${endTime}`
   },
 
   updateNavbar ({ commit }, payload) {
